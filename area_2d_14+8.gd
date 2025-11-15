b48f20d34c6006c0161ce620aa229d71680828b4
Miguella297 <mikeivanov2010@gmail.com>
2025-11-15 15:27:36 -0500

    NEW SCENES

--- area_2d_14+8.gd
+++ area_2d_14+8.gd
@@ -1,14 +1,14 @@
 extends Area2D
 
-# Path to the new scene you want to load
-@export var target_scene_path: String = "res://level+-level+2.tscn"
+# Path to the new scene yoh\u want to load
+@export var target_scene_path: String = "res://Deathscene14.tscn"
 
 func _ready():
 	# Connect the signal (if not already connected in the editor)
 	connect("body_entered", _on_body_entered)
 
 func _on_body_entered(body):
-	# Check if the body is in your player (assuming its a KinematicBody2D or a Characterbody2D)
+	# Check if the body is in your player (assuming its a Kinematicbody2D or a Characterbody2D)
 	
 	if body.is_in_group("player"):
 		call_deferred("_fix")
