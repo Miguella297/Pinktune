7d50a3756cf3dd2a4d9bc70979d8b74ced8bd9be
Miguella297 <mikeivanov2010@gmail.com>
2025-11-17 22:40:23 -0500

    finishing spikes for bonus level 3

--- bonus_2_3.gd
+++ bonus_2_3.gd
@@ -1,7 +1,10 @@
 extends Node2D
 
+
+
 func _on_button_2_pressed() -> void:
 	get_tree().change_scene_to_file("node_2d.tscn")
 
-func _on_button_pressed() -> void:
+
+func _on_button_1234_pressed() -> void:
 	get_tree().change_scene_to_file("bonus3.tscn")
