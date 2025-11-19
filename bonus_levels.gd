02a168fc2ca0ca7331ad6492760486f3e19efa8d
Miguella297 <mikeivanov2010@gmail.com>
2025-11-19 00:07:40 -0500

    continuing further developed enemies for bonus level 3

--- bonus_levels.gd
+++ bonus_levels.gd
@@ -8,3 +8,7 @@ func _on_button_2_pressed() -> void:
 
 func _on_button_3_pressed() -> void:
 	get_tree().change_scene_to_file("bonus2.tscn")
+
+
+func _on_button_4_pressed() -> void:
+	get_tree().change_scene_to_file("bonus3.tscn")
