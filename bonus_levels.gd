da217ff4e92d942e80c241d02a65460a45137704
Miguella297 <mikeivanov2010@gmail.com>
2025-11-21 20:06:11 -0500

    GAME FINISHED AT 75 HOURS AND 43 MINUTES

--- bonus_levels.gd
+++ bonus_levels.gd
@@ -12,3 +12,6 @@ func _on_button_3_pressed() -> void:
 
 func _on_button_4_pressed() -> void:
 	get_tree().change_scene_to_file("bonus3.tscn")
+
+func _on_button_5_pressed() -> void:
+	get_tree().change_scene_to_file("bonus4.tscn")
