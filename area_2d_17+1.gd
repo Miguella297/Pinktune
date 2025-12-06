17f6ff21e59defba449f87ca1740bf79dd857b8d
Miguella297 <mikeivanov2010@gmail.com>
2025-12-06 14:24:36 -0500

    working on levels to cover tickets

--- area_2d_17+1.gd
+++ area_2d_17+1.gd
@@ -1,7 +1,7 @@
 extends Area2D
 
 # Path to the new scene you want to load
-@export var target_scene_path : String = "res://greatjob.tscn"
+@export var target_scene_path : String = "res://bonus4-5.tscn"
 
 func _ready():
 	# Connect the signal (if not already connected in the editor)
